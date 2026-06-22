d72500a4e5a205db5a792fd56ff7d9bf50f30028
lyz1202dt <cloud1202@qq.com>
2026-06-22 16:40:12 +0800

    增加launch文件启动两个视觉识别任务

---
+++ src/launch_pack/launch/vision_task.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='arm',
+            executable='arm_node',
+            name='arm_vision_task',
+            output='screen',
+        ),
+        Node(
+            package='arithmetic_problem',
+            executable='arithmetic_problem',
+            name='arithmetic_problem_vision_task',
+            output='screen',
+        ),
+    ])
